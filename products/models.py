cd29e5eaa06ca1bdaae402e996328caeec90d4e2
Shadyxstep <laumorgan.14@gmail.com>
2021-04-14 12:26:25 +0000

    adjusted model structure in products, seperated accessories from clothing, added sizes value to clothing

--- products/models.py
+++ products/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -12,7 +16,35 @@ class Category(models.Model):
         return self.friendly_name
 
 
-class Product(models.Model):
+class Clothing(models.Model):
+    class Meta:
+        verbose_name_plural = 'Clothing'
+
+    clothing_size = (
+        ('S', 'Small'),
+        ('M', 'Medium'),
+        ('L', 'Large'),
+    )
+    category = models.ForeignKey(
+        'Category', null=True, blank=True, on_delete=models.SET_NULL)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    description = models.TextField()
+    size = models.CharField(max_length=1, default='choose your size', choices=clothing_size)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    rating = models.DecimalField(
+        max_digits=6, decimal_places=2, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Accessories(models.Model):
+    class Meta:
+        verbose_name_plural = 'Accessories'
+
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
@@ -26,3 +58,6 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
