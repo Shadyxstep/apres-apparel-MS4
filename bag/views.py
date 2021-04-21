b6753874590a359538f8efba82dbfd5502e317b3
Shadyxstep <laumorgan.14@gmail.com>
2021-04-21 10:06:53 +0000

    updated shopping bag template & functionality

--- bag/views.py
+++ bag/views.py
@@ -14,12 +14,11 @@ def add_to_bag(request, item_id):
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
+    """ Adds item to bag or updates quantity if item in bag already exists"""
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
     else:
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
-    
