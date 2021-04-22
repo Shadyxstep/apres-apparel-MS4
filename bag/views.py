35360c4d017048931144e5844633b737f6543857
Shadyxstep <laumorgan.14@gmail.com>
2021-04-22 11:22:43 +0000

    added product size logic functionality to contexts & bag views

--- bag/views.py
+++ bag/views.py
@@ -12,13 +12,26 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
+    """ Maintains item_id for each item in bag but also tracks sizes of items in bag """
     """ Adds item to bag or updates quantity if item in bag already exists"""
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
