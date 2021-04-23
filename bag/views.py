c7061b34b7aa60af82aca935a4ef4f2b734f5d7d
Shadyxstep <laumorgan.14@gmail.com>
2021-04-23 14:39:04 +0000

    added toast bag preview, css & additional toast messages

--- bag/views.py
+++ bag/views.py
@@ -31,10 +31,10 @@ def add_to_bag(request, item_id):
                 messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
                 bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'{product.name} ({size.upper()}) successfully added to your bag')
+                messages.success(request, f'{product.name} ({size.upper()}) successfully added to your bag!')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'{product.name} ({size.upper()}) successfully added to your bag')
+            messages.success(request, f'{product.name} ({size.upper()}) successfully added to your bag!')
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
