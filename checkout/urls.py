0df10bb9dbf515e7becac044b91654f8cec10c9f
Shadyxstep <laumorgan.14@gmail.com>
2021-04-24 16:49:18 +0000

    added checkout success logic

--- checkout/urls.py
+++ checkout/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.checkout, name='checkout')
+    path('', views.checkout, name='checkout'),
+    path('checkout_successful/<order_number>', views.checkout_successful, name='checkout_successful'),
 ]
