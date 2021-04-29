97c28918f6aba1223b703dcd8c41b5c6bde9f553
Shadyxstep <laumorgan.14@gmail.com>
2021-04-29 19:21:13 +0000

    Removed secret key & set debug

--- apres_apparel/settings.py
+++ apres_apparel/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'lh+%7d3btlhb4$wqzd*#-nfn1#0b7smz-cy&05)e9=8kk*u_m%'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['apres-apparel.herokuapp.com', 'localhost']
 
