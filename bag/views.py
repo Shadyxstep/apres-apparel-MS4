cdfaafb1a97bd98d2ee1abb6602a48d9a0813c09
Shadyxstep <laumorgan.14@gmail.com>
2021-05-01 10:35:36 +0000

    refactoring python files in bag/checkout apps

--- bag/views.py
+++ bag/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import (
     render, redirect, reverse, HttpResponse, get_object_or_404
     )
 from django.contrib import messages
-
 from products.models import Product
 
-# Create your views here.
 
 def view_bag(request):
     """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
+    
+    """ Add a quantity of product to the shopping bag"""
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
@@ -88,7 +88,9 @@ def update_bag(request, item_id):
                      quantity updated to {bag[item_id]}')
         else:
             bag.pop(item_id)
-            messages.success(request, f'{product.name} removed from your bag')
+            messages.success(
+                request, f'{product.name}\
+                     removed from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -108,14 +110,19 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_size'][size]
             if not bag[item_id]['items_by_size']:
                 bag.pop(item_id)
-            messages.success(request, f'Removed size {size.upper()} {product.name} from your bag')
+            messages.success(
+                request, f'Removed size {size.upper()}\
+                     {product.name} from your bag')
         else:
             bag.pop(item_id)
-            messages.success(request, f'{product.name} removed from your bag')
+            messages.success(
+                request, f'{product.name}\
+                     removed from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
         
     except Exception as e:
-        messages.error(request, f'Error removing item: {e}')
+        messages.error(
+            request, f'Error removing item: {e}')
         return HttpResponse(status=500)
